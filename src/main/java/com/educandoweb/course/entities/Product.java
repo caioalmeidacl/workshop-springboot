0c81ffe12252301e70bcdfd4d25f2711fa58c1ef
CAIO ALMEIDA <caioalmeidaacl@gmail.com>
2023-12-27 11:47:39 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -25,6 +26,8 @@ public class Product implements Serializable {
             , inverseJoinColumns = @JoinColumn(name = "category_id")) // Foreign key from category
     // relation n -> n
     private Set<Category> categories = new HashSet<>();
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){}
     public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -77,6 +80,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x: items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
