5d420f558f64e0ee888bebb609bd56d5e5b79f71
CAIO ALMEIDA <caioalmeidaacl@gmail.com>
2023-12-28 14:46:20 -0300

    User Delete

--- src/main/java/com/educandoweb/course/resources/UserResources.java
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -30,10 +30,16 @@ public class UserResources {
     }
 
     @PostMapping
-    public ResponseEntity insert(@RequestBody User obj){
+    public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
